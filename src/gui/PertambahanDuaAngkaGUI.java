d1b7bbb680df9c4c98932572f90a0bf37c7cbbcd
zaieny <117129078+zaieny@users.noreply.github.com>
2022-11-13 10:55:09 +0700

    Menambah kan coding hapus

--- src/gui/PertambahanDuaAngkaGUI.java
+++ src/gui/PertambahanDuaAngkaGUI.java
@@ -130,6 +130,11 @@ public class PertambahanDuaAngkaGUI extends javax.swing.JFrame {
         );
 
         hapusBtn.setText("Hapus");
+        hapusBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                hapusBtnActionPerformed(evt);
+            }
+        });
 
         keluarBtn.setText("Keluar");
 
@@ -205,6 +210,15 @@ public class PertambahanDuaAngkaGUI extends javax.swing.JFrame {
        }
        
     }//GEN-LAST:event_tambahBtnActionPerformed
+
+    private void hapusBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_hapusBtnActionPerformed
+
+     angkaPertamaTF.setText("");
+     angkaKeduaTF.setText(null);
+     hasilTF.setText("");
+     
+     angkaPertamaTF.requestFocus();
+    }//GEN-LAST:event_hapusBtnActionPerformed
 
     /**
      * @param args the command line arguments
